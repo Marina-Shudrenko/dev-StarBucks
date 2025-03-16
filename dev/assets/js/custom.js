ef5a7401b9a6329b27d87c350dbb7497f5ffff85
Marina <marinasudrenko@gmail.com>
2025-03-16 23:40:36 +0200

    feat: add modal window

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -5,3 +5,30 @@ burger.addEventListener('click', ()=> {
     burger.classList.toggle('active')
     mobileContainer.classList.toggle('show')
 })
+
+const btnTriggerProcess = document.querySelector('[data-trigger-modal="process"]')
+const overlay = document.querySelector('.overlay')
+const body = document.querySelector('body')
+const html = document.querySelector('html')
+const modal = document.querySelector('.modal')
+const closeButton = document.querySelector('.modal button')
+
+
+function showModal(event) {
+overlay.classList.add('show')
+modal.classList.add('show')
+body.classList.add('overflow-hidden')
+html.classList.add('overflow-hidden')
+event.preventDefault()
+}
+
+function closeModal() {
+    overlay.classList.remove('show')
+    modal.classList.remove('show')
+    body.classList.remove('overflow-hidden')
+    html.classList.remove('overflow-hidden')
+    }
+
+
+btnTriggerProcess.addEventListener('click', showModal)
+closeButton.addEventListener('click', closeModal)
