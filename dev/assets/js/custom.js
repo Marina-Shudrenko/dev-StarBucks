dd1bc85476dd203a577fbabdcb2a39300f0c3b45
Marina <marinasudrenko@gmail.com>
2025-03-24 16:26:34 +0200

    feat: added video player in modal window

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,10 +1,78 @@
+//burger-menu
+
 const burger = document.querySelector('#btn-burger')
 const mobileContainer = document.querySelector('#mobile-container')
 
 burger.addEventListener('click', () => {
-    burger.classList.toggle('active')
-    mobileContainer.classList.toggle('show')
+    burger.classList.toggle('active');
+    mobileContainer.classList.toggle('show');
 })
+
+//video-button
+
+const processVideo = document.querySelector('#video');
+const play = document.querySelector('#play');
+const pause = document.querySelector('#pause');
+const stop = document.querySelector('#stop');
+
+function playVideo() {
+    processVideo.play();
+}
+
+function pauseVideo() {
+    processVideo.pause();
+}
+
+function stopVideo() {
+    processVideo.pause();
+    processVideo.currentTime = 0;
+}
+
+play.addEventListener('click', () => {
+    playVideo();
+})
+
+pause.addEventListener('click', () => {
+    pauseVideo();
+})
+
+stop.addEventListener('click', () => {
+    stopVideo();
+})
+
+
+
+//currentTime : html, css, js
+
+const html = document.querySelector('#html');
+const css = document.querySelector('#css');
+const js = document.querySelector('#js');
+
+function timeHTML() {
+    video.currentTime = 69;
+}
+
+function timeCSS() {
+    video.currentTime = 109;
+}
+
+function timeJS() {
+    video.currentTime = 202;
+}
+
+html.addEventListener('click', () => {
+    timeHTML();
+})
+
+css.addEventListener('click', () => {
+    timeCSS();
+})
+
+js.addEventListener('click', () => {
+    timeJS();
+})
+
+
 
 // modal 
 
@@ -12,12 +80,10 @@ const modalTriggers = document.querySelectorAll('[data-trigger-modal]');
 const modalClose = document.querySelectorAll('[data-close-modal]');
 const overlay = document.querySelector('.overlay');
 const body = document.querySelector('body');
-const html = document.querySelector('html');
 
 function showModal() {
     overlay.classList.add('show');
     body.classList.add('overflow-hidden');
-    html.classList.add('overflow-hidden');
 }
 
 function closeModal() {
@@ -26,7 +92,7 @@ function closeModal() {
     openModal.classList.remove('show');
     overlay.classList.remove('show');
     body.classList.remove('overflow-hidden');
-    html.classList.remove('overflow-hidden');
+    stopVideo();
 }
 
 modalTriggers.forEach(item => {
@@ -37,12 +103,18 @@ modalTriggers.forEach(item => {
         const modal = document.querySelector('#' + dataTrigger);
 
         showModal();
-        modal.classList.add('show');
+
+        if (modal.classList.contains('video-process')) {
+            modal.classList.add('show');
+            playVideo();
+        } else {
+            modal.classList.add('show');
+        }
     })
 })
 
 modalClose.forEach(close => {
-    close.addEventListener('click', closeModal)
+    close.addEventListener('click', closeModal);
 })
 
 
